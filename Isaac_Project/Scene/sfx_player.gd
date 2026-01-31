6c166064babb1c6b002dd0bcc45e3a940bc47f44
Kay Faraday <yatagarasu163@gmail.com>
2026-01-31 22:53:06 +0800

    Everything that hath breath praise the Lord
    Worship Christ with the best of your portions.
    I know I won't forget all He's done.

--- Isaac_Project/Scene/sfx_player.gd
+++ Isaac_Project/Scene/sfx_player.gd
@@ -1,20 +1,30 @@
-extends AudioStreamPlayer2D
+extends Node2D;
 
 # Called when the node enters the scene tree for the first time.
-@onready var audio_player: AudioStreamPlayer2D = $".";
 @onready var sfx_volume_bus = AudioServer.get_bus_index("SFX");
 const sfx_map:= {
-	"hurt": "res://Data/SFX/Hurt.wav",
-	"mouse_hover": "res://Data/SFX/Hover.wav",
-	"footsteps": "res://Data/SFX/Footstep.wav",
-	"coin_pickup": "res://Data/SFX/CoinPickup.wav",
-	"mask_pickup": "res://Data/SFX/MaskPickup.wav",
-	"select": "res://Data/SFX/Select.wav",
-	"spear_throw": "res://Data/SFX/Spear Throw.wav"
+	"hurt": preload("res://Data/SFX/Hurt.wav"),
+	"mouse_hover": preload("res://Data/SFX/Hover.wav"),
+	"footsteps": preload("res://Data/SFX/Footstep.wav"),
+	"coin_pickup": preload("res://Data/SFX/CoinPickup.wav"),
+	"mask_pickup": preload("res://Data/SFX/MaskPickup.wav"),
+	"select": preload("res://Data/SFX/Select.wav"),
+	"spear_throw": preload("res://Data/SFX/Spear Throw.wav")
 }
 
 func _play_sfx(sfx_name):
 	if sfx_map.has(sfx_name):
-		var next_stream = load(sfx_map[sfx_name]);
-		audio_player.stream = next_stream;
-		audio_player.play();
+		run_sfx(sfx_map[sfx_name]);
+
+
+func run_sfx(stream: AudioStream):
+	if stream == null:
+		return;
+
+	var player := AudioStreamPlayer.new()
+	player.stream = stream
+	player.bus = "SFX"
+	add_child(player)
+	player.pitch_scale = randf(0.90, 1.10);
+	player.play()
+	player.finished.connect(player.queue_free)
